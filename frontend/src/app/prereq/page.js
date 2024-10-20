499abd5949bbec1ba326676780f61585e5fcc287
Aryaman Kukal <aryamankukal@Aryamans-MacBook-Pro.local>
2024-10-19 22:24:18 -0700

    changed scroll bar color

--- frontend/src/app/prereq/page.js
+++ frontend/src/app/prereq/page.js
@@ -21,19 +21,20 @@ export default function Prereq() {
   };
 
   return (
-    <main className="text-white/60 bg-slate-900 w-screen h-screen px-72 mx-auto pt-40">
+    <main className="text-blue-300 bg-slate-900 w-screen h-screen px-72 mx-auto pt-40">
       <div className="flex flex-col gap-4 text-2xl font-bold mb-8">
         <div className="flex justify-between">
           <h2 className="text-3xl font-bold">
-            My <span className="text-orange-500">skill level</span> is...
+            My <span className="text-blue-300">skill level</span> is...
           </h2>
-          <p className="text-orange-500">{translateSkill(skill)}</p>
+          <p className="text-blue-300">{translateSkill(skill)}</p>
         </div>
         <Slider
           onValueChange={(v) => setSkill(v)}
           defaultValue={[40]}
           max={100}
           step={20}
+          className="[&_[role=slider]]:bg-blue-300 [&_[role=slider]]:text-blue-300"
         />
       </div>
       {skill !== null ? (
@@ -44,9 +45,9 @@ export default function Prereq() {
           className="mb-8"
         >
           <h2 className="text-3xl font-bold">
-            Important <span className="text-orange-500">Concepts</span>
+            Important <span className="text-blue-300">Concepts</span>
           </h2>
-          <p className="">
+          <p className="text-blue-300">
             ttest tesintesintse tseintesintseot seo tkse tjksentjk
           </p>
         </motion.div>
@@ -59,9 +60,9 @@ export default function Prereq() {
           className="mb-8"
         >
           <h2 className="text-3xl font-bold">
-            Relevant <span className="text-orange-500">Sources</span>
+            Relevant <span className="text-blue-300">Sources</span>
           </h2>
-          <p className="">use aryamans code for scraping youtube links</p>
+          <p className="text-blue-300">use aryamans code for scraping youtube links</p>
         </motion.div>
       ) : null}
     </main>
