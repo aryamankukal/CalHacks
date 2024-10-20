f60c2d066f6b9ce948e78aef46d63322f8ef3d36
Alex Luo <alexluo92823@gmail.com>
2024-10-19 21:14:51 -0700

    change dropdown to slider

--- frontend/src/app/prereq/page.js
+++ frontend/src/app/prereq/page.js
@@ -1,45 +1,41 @@
 "use client";
 
 import { useState } from "react";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/app/components/ui/select";
+import { Slider } from "@/app/components/ui/slider";
 import { motion } from "framer-motion";
 
 export default function Prereq() {
-  const [skill, setSkill] = useState(null);
+  const [skill, setSkill] = useState(50);
 
   return (
-    <main className="text-white bg-slate-900 w-screen h-screen px-72 mx-auto py-40">
-      <div className="flex items-center gap-4 text-2xl font-bold mb-4">
-        <h2 className="text-2xl font-bold">I&apos;m a(n) </h2>
-        <Select className="" onValueChange={(v) => setSkill(v)}>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="..." />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="beginner">beginner</SelectItem>
-              <SelectItem value="intermediate">intermediate</SelectItem>
-              <SelectItem value="expert">expert</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+    <main className="text-white/60 bg-slate-900 w-screen h-screen px-72 mx-auto pt-40">
+      <div className="flex flex-col gap-4 text-2xl font-bold mb-8">
+        <div className="flex justify-between">
+          <h2 className="text-3xl font-bold">
+            My <span className="text-orange-500">skill level</span> is...
+          </h2>
+          <p className="text-orange-500">{skill}</p>
+        </div>
+        <Slider
+          onValueChange={(v) => setSkill(v)}
+          defaultValue={[50]}
+          max={100}
+          step={1}
+        />
       </div>
       {skill !== null ? (
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.4 }}
-          className="mb-4"
+          className="mb-8"
         >
-          <h2 className="text-2xl font-bold">Intro</h2>
-          <p className="">- summarize keys words with ai</p>
+          <h2 className="text-3xl font-bold">
+            Important <span className="text-orange-500">Concepts</span>
+          </h2>
+          <p className="">
+            ttest tesintesintse tseintesintseot seo tkse tjksentjk
+          </p>
         </motion.div>
       ) : null}
       {skill !== null ? (
@@ -47,9 +43,11 @@ export default function Prereq() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.8 }}
-          className="mb-4"
+          className="mb-8"
         >
-          <h2 className="text-2xl font-bold">Sources</h2>
+          <h2 className="text-3xl font-bold">
+            Relevant <span className="text-orange-500">Sources</span>
+          </h2>
           <p className="">use aryamans code for scraping youtube links</p>
         </motion.div>
       ) : null}
